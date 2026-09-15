1f5084f3d4346599fd20634a83212e2d67bbd486
rixicz <patrik.smidoch@hotmail.com>
2026-09-15 12:02:13 +0200

    implemented push for the four segment pointers

--- part_2/07/VM_Translator.py
+++ part_2/07/VM_Translator.py
@@ -9,31 +9,71 @@ def reader(filename: str):
         
         return commands
 
-def parser(commands: list):
+def code_constant(command_list: list, asm_ins: list):
+    asm_ins.append(f"@{command_list[2]}")
+    asm_ins.append("D=A")
+    asm_ins.append("@SP")
+    asm_ins.append("A=M")
+    asm_ins.append("M=D")
+    asm_ins.append("@SP")
+    asm_ins.append("M=M+1")
+
+def code_arg_local_this_that(c_list: list, asm_ins, pointer: str):
+    asm_ins.append(f"@{c_list[2]}")
+    asm_ins.append("D=A")
+    asm_ins.append(pointer)
+    asm_ins.append("A=D+M")
+    asm_ins.append("D=M")
+    asm_ins.append("@SP")
+    asm_ins.append("A=M")
+    asm_ins.append("M=D")
+    asm_ins.append("@SP")
+    asm_ins.append("M=M+1")
+
+def coder(commands: list):
     asm_ins = []
     for c in commands:
         c = c.strip()
         c_list = c.split(" ")
-        comment = "// " + c
+        asm_ins.append("// " + c + "\n")
         if c_list[0] == "push":
             if c_list[1] == "constant":
-                print(c_list)
-                asm_ins.append(comment + "\n")
-                asm_ins.append(f"@{c_list[2]}")
-                asm_ins.append("D=A")
-                asm_ins.append("@SP")
-                asm_ins.append("A=M")
-                asm_ins.append("M=D")
-                asm_ins.append("@SP")
-                asm_ins.append("M=M+1")
-                asm_ins.append("\n")
+                code_constant(c_list, asm_ins)            
+            
+            elif c_list[1] == "argument":
+                code_arg_local_this_that(c_list, asm_ins, "@ARG")
+
+            elif c_list[1] == "local":
+                code_arg_local_this_that(c_list, asm_ins, "@LOC")
+
+            elif c_list[1] == "this":
+                code_arg_local_this_that(c_list, asm_ins, "@THIS")
+
+            elif c_list[1] == "that":
+                code_arg_local_this_that(c_list, asm_ins, "@THAT")
+        
+        if c_list[0] == "pop":
+            
+            if c_list[1] == "argument":
+                pass
+
+            elif c_list[1] == "local":
+                pass
+
+            elif c_list[1] == "this":
+                pass
+
+            elif c_list[1] == "that":
+                pass
+        
+        asm_ins.append("\n")
 
     return asm_ins
 
 filename = input("Please specify the filename: ")
 
 vm_commands = reader(filename)
-asm_instructions = parser(vm_commands)
+asm_instructions = coder(vm_commands)
 
 with open("asm_programs/" + filename + ".asm", "w") as file:
     for ins in asm_instructions:
--- part_2/07/asm_programs/BasicTest.asm
+++ part_2/07/asm_programs/BasicTest.asm
@@ -9,6 +9,10 @@ M=D
 M=M+1
 
 
+// pop local 0
+
+
+
 // push constant 21
 
 @21
@@ -31,6 +35,14 @@ M=D
 M=M+1
 
 
+// pop argument 2
+
+
+
+// pop argument 1
+
+
+
 // push constant 36
 
 @36
@@ -40,6 +52,10 @@ A=M
 M=D
 @SP
 M=M+1
+
+
+// pop this 6
+
 
 
 // push constant 42
@@ -64,6 +80,14 @@ M=D
 M=M+1
 
 
+// pop that 5
+
+
+
+// pop that 2
+
+
+
 // push constant 510
 
 @510
@@ -73,5 +97,103 @@ A=M
 M=D
 @SP
 M=M+1
+
+
+// pop temp 6
+
+
+
+// push local 0
+
+@0
+D=A
+@LOC
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+
+// push that 5
+
+@5
+D=A
+@THAT
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+
+// add
+
+
+
+// push argument 1
+
+@1
+D=A
+@ARG
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+
+// sub
+
+
+
+// push this 6
+
+@6
+D=A
+@THIS
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+
+// push this 6
+
+@6
+D=A
+@THIS
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+
+// add
+
+
+
+// sub
+
+
+
+// push temp 6
+
+
+
+// add
+
 
 
